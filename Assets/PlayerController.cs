8d461427fa3ccc29a77c73ce05d3cbb00a4f713e
Alex <133706872+atpugh20@users.noreply.github.com>
2024-07-17 21:34:02 -0700

    Adjusted collision detection of player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float speed = 20f;
     private float horizontalInput;
     private bool jumpInput;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
-    private bool isJumping = false;
+    private BoxCollider2D fCol;
+    // Constants
+    private bool onGround = false;
+    public float speed = 20f;
+    public float jumpVelocity = 35;
+    
     
     void Start() {
         // Start is called before the first frame update
-
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        fCol = GameObject.Find("FeetCollider").GetComponent<BoxCollider2D>();
+
     }
 
     private void FixedUpdate() {
         // Runs every 60 frames
 
         horizontalInput = Input.GetAxisRaw("Horizontal");
+        // flip sprite based on movement direction
         if (horizontalInput > 0) {
             sr.flipX = true;
-        }
-        else if (horizontalInput < 0) {
+        } else if (horizontalInput < 0) {
             sr.flipX = false;
         }
         float horizontalMovement = horizontalInput * speed * Time.deltaTime;
@@ -36,11 +41,15 @@ public class PlayerController : MonoBehaviour {
         // Runs once every frame (machine dependent)
 
         jumpInput = Input.GetButton("Jump");
-        if (jumpInput && !isJumping)
-        {
-            isJumping = true;
-            rb.velocity = new Vector2(rb.velocity.x, 35);
+        if (jumpInput && onGround) {
+            onGround = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
         }
-        if (rb.velocity.y == 0) isJumping = false; 
+       
+    }
+
+    void OnCollisionStay2D(Collision2D col) {
+        if (col.gameObject.tag == "Ground") onGround = true;
+        print(fCol);
     }
 }
