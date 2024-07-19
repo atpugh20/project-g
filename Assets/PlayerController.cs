9d9fde5fbcc938587775e5fa615cc67f968b990c
Alex <133706872+atpugh20@users.noreply.github.com>
2024-07-19 15:45:29 -0700

    sac

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,83 +1,188 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    private float horizontalInput;
-    private bool jumpInput;
-    private bool boostInput;
-    private Rigidbody2D rb;
-    private CapsuleCollider2D[] capsules;
-    private CapsuleCollider2D fCol;
-    private CapsuleCollider2D wCol;
-    private BoxCollider2D bc;
-    private TrailRenderer tr;
-    private Animator animator;
-    public Transform transform;
-    private bool onGround = false;
-    private bool isBoosted = false;
-    public float speed = 1000f;
-    public float jumpVelocity = 35f;
-    public float boostStrength = 5f;
-    public float deadZone = 0.05f;
+namespace TController {
+    /// <summary>
+    /// Hey!
+    /// Tarodev here. I built this controller as there was a severe lack of quality & free 2D controllers out there.
+    /// I have a premium version on Patreon, which has every feature you'd expect from a polished controller. Link: https://www.patreon.com/tarodev
+    /// You can play and compete for best times here: https://tarodev.itch.io/extended-ultimate-2d-controller
+    /// If you hve any questions or would like to brag about your score, come to discord: https://discord.gg/tarodev
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+    public class PlayerController : MonoBehaviour, IPlayerController {
+        [SerializeField] private ScriptableStats _stats;
+        private Rigidbody2D _rb;
+        private CapsuleCollider2D _col;
+        private FrameInput _frameInput;
+        private Vector2 _frameVelocity;
+        private bool _cachedQueryStartInColliders;
 
-    // Before the first frame update
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        capsules = GetComponents<CapsuleCollider2D>();
-        wCol = capsules[0];
-        fCol = capsules[1];
-        tr = GetComponent<TrailRenderer>();
-        animator = GetComponent<Animator>();
-        transform = GetComponent<Transform>();
+        #region Interface
+
+        public Vector2 FrameInput => _frameInput.Move;
+        public event Action<bool, float> GroundedChanged;
+        public event Action Jumped;
+
+        #endregion
+
+        private float _time;
+
+        private void Awake() {
+            _rb = GetComponent<Rigidbody2D>();
+            _col = GetComponent<CapsuleCollider2D>();
+
+            _cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
+        }
+
+        private void Update() {
+            _time += Time.deltaTime;
+            GatherInput();
+        }
+
+        private void GatherInput() {
+            _frameInput = new FrameInput {
+                JumpDown = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.C),
+                JumpHeld = Input.GetButton("Jump") || Input.GetKey(KeyCode.C),
+                Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))
+            };
+
+            if (_stats.SnapInput) {
+                _frameInput.Move.x = Mathf.Abs(_frameInput.Move.x) < _stats.HorizontalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.x);
+                _frameInput.Move.y = Mathf.Abs(_frameInput.Move.y) < _stats.VerticalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.y);
+            }
+
+            if (_frameInput.JumpDown) {
+                _jumpToConsume = true;
+                _timeJumpWasPressed = _time;
+            }
+        }
+
+        private void FixedUpdate() {
+            CheckCollisions();
+
+            HandleJump();
+            HandleDirection();
+            HandleGravity();
+
+            ApplyMovement();
+        }
+
+        #region Collisions
+
+        private float _frameLeftGrounded = float.MinValue;
+        private bool _grounded;
+
+        private void CheckCollisions() {
+            Physics2D.queriesStartInColliders = false;
+
+            // Ground and Ceiling
+            bool groundHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, _stats.GrounderDistance, ~_stats.PlayerLayer);
+            bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
+
+            // Hit a Ceiling
+            if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
+
+            // Landed on the Ground
+            if (!_grounded && groundHit) {
+                _grounded = true;
+                _coyoteUsable = true;
+                _bufferedJumpUsable = true;
+                _endedJumpEarly = false;
+                GroundedChanged?.Invoke(true, Mathf.Abs(_frameVelocity.y));
+            }
+            // Left the Ground
+            else if (_grounded && !groundHit) {
+                _grounded = false;
+                _frameLeftGrounded = _time;
+                GroundedChanged?.Invoke(false, 0);
+            }
+
+            Physics2D.queriesStartInColliders = _cachedQueryStartInColliders;
+        }
+
+        #endregion
+
+
+        #region Jumping
+
+        private bool _jumpToConsume;
+        private bool _bufferedJumpUsable;
+        private bool _endedJumpEarly;
+        private bool _coyoteUsable;
+        private float _timeJumpWasPressed;
+
+        private bool HasBufferedJump => _bufferedJumpUsable && _time < _timeJumpWasPressed + _stats.JumpBuffer;
+        private bool CanUseCoyote => _coyoteUsable && !_grounded && _time < _frameLeftGrounded + _stats.CoyoteTime;
+
+        private void HandleJump() {
+            if (!_endedJumpEarly && !_grounded && !_frameInput.JumpHeld && _rb.velocity.y > 0) _endedJumpEarly = true;
+
+            if (!_jumpToConsume && !HasBufferedJump) return;
+
+            if (_grounded || CanUseCoyote) ExecuteJump();
+
+            _jumpToConsume = false;
+        }
+
+        private void ExecuteJump() {
+            _endedJumpEarly = false;
+            _timeJumpWasPressed = 0;
+            _bufferedJumpUsable = false;
+            _coyoteUsable = false;
+            _frameVelocity.y = _stats.JumpPower;
+            Jumped?.Invoke();
+        }
+
+        #endregion
+
+        #region Horizontal
+
+        private void HandleDirection() {
+            if (_frameInput.Move.x == 0) {
+                var deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
+                _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
+            } else {
+                _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
+            }
+        }
+
+        #endregion
+
+        #region Gravity
+
+        private void HandleGravity() {
+            if (_grounded && _frameVelocity.y <= 0f) {
+                _frameVelocity.y = _stats.GroundingForce;
+            } else {
+                var inAirGravity = _stats.FallAcceleration;
+                if (_endedJumpEarly && _frameVelocity.y > 0) inAirGravity *= _stats.JumpEndEarlyGravityModifier;
+                _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -_stats.MaxFallSpeed, inAirGravity * Time.fixedDeltaTime);
+            }
+        }
+
+        #endregion
+
+        private void ApplyMovement() => _rb.velocity = _frameVelocity;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_stats == null) Debug.LogWarning("Please assign a ScriptableStats asset to the Player Controller's Stats slot", this);
+        }
+#endif
     }
 
-    // Runs every 60 frames
-    private void FixedUpdate() {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        if (-deadZone < horizontalInput && horizontalInput < deadZone) {
-            animator.speed = 1;
-            animator.SetBool("isRunning", false);
-        } else {
-            animator.speed = Math.Abs(horizontalInput);
-            animator.SetBool("isRunning", true);
-        }
-        if (horizontalInput > 0) {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-        else if (horizontalInput < 0) {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-        float horizontalMovement = horizontalInput * speed * Time.deltaTime;
-        rb.velocity = new Vector2(horizontalMovement, rb.velocity.y);
+    public struct FrameInput {
+        public bool JumpDown;
+        public bool JumpHeld;
+        public Vector2 Move;
     }
 
-    // Runs once every frame (machine dependent)
-    void Update() {
-        jumpInput = Input.GetButton("Jump");
-        boostInput = Input.GetButton("Fire3");
-        if (jumpInput && onGround) {
-            animator.SetBool("isJumping", true);
-            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
-        }
-        if (boostInput && !isBoosted) {
-            tr.emitting = true;
-            isBoosted = true;
-            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity); 
-        }
-    }
+    public interface IPlayerController {
+        public event Action<bool, float> GroundedChanged;
 
-    void OnCollisionStay2D(Collision2D col) {
-        if (col.otherCollider == fCol) {
-            onGround = true;
-            isBoosted = false;
-            tr.emitting = false;
-            animator.SetBool("isJumping", false);
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D col) {
-        onGround = false;
+        public event Action Jumped;
+        public Vector2 FrameInput { get; }
     }
 }
