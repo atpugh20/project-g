5bc790d6cc4c20b5fb4a878dc761737c5cfa0fc2
buzzulbee <buzzulbee@gmail.com>
2024-07-22 22:44:14 -0400

    goop animation and crumbly animation

--- Assets/CrumblingPlatform.cs
+++ Assets/CrumblingPlatform.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class CrumblingPlatform : MonoBehaviour
 {
     public float crumbleDelay = 1f;
+    Animator animator;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            animator.SetBool("isOnPlatform", true);
             Invoke("Crumble", crumbleDelay);
         }
     }
 
     private void Crumble()
     {
+        animator.Play("CrumblePlat1");
         Destroy(gameObject);
     }
 }
