b9c60436267d636a1d9cd7635b9074c843cb3e49
Alex <133706872+atpugh20@users.noreply.github.com>
2024-07-26 03:49:32 -0700

    Finished Initial Watergun

--- Assets/FlameDashActivate.cs
+++ Assets/FlameDashActivate.cs
@@ -2,25 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlameDashActivate : MonoBehaviour
-{
-
+public class FlameDashActivate : MonoBehaviour{
     private Animator animator;
 
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        print(gameObject);
-    }
+    void Start() => animator = GetComponent<Animator>();
+    public void FlameDash() => animator.SetBool("isDashing", true);
 
-    public void FlameDash()
-    {
-        animator.SetBool("isDashing", true);
-    }
-
-    public void FlameDashOff()
-    {
-        animator.SetBool("isDashing", false);
-    }
-
+    public void FlameDashOff() => animator.SetBool("isDashing", false);
 }
--- Assets/WaterBulletController.cs
+++ Assets/WaterBulletController.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterBulletController : MonoBehaviour {
-    Vector2 Move;
-    float InitialVector;
+    
     private Rigidbody2D _rb;
+    private Transform _transform;
+    private Animator _anim;
+    private Vector2 _move;
+    private bool _col = false;
+    public int Speed = 30;
 
     // Start is called before the first frame update
     void Start() {
         _rb = GetComponent<Rigidbody2D>();
-        Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        print(transform.right);
-        _rb.velocity = transform.right;
-        print(_rb.velocity);
+        _transform = GetComponent<Transform>();
+        _anim = GetComponentInChildren<Animator>();
+        _move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _anim.SetBool("isMoving", true);
+        float newAngle = Mathf.Atan2(_move.y, _move.x) * Mathf.Rad2Deg;
+        _transform.eulerAngles = new Vector3(0f, _transform.eulerAngles.y, newAngle);
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (!_col) _rb.velocity = _move * Speed;
     }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Player")) return;
+        _col = true;
+        _rb.velocity = Vector2.zero;
+        _anim.SetBool("isMoving", false);
+    }
+
+
+    public void Kill() => Destroy(gameObject);
 }
