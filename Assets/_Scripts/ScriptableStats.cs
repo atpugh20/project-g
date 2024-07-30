30a90d50190cdec5e18f1962eb5bb7ef36194ba7
Alex <133706872+atpugh20@users.noreply.github.com>
2024-07-29 20:42:15 -0700

    updated lvl2 colors

--- Assets/_Scripts/ScriptableStats.cs
+++ Assets/_Scripts/ScriptableStats.cs
@@ -10,34 +10,34 @@ namespace TController
         public LayerMask PlayerLayer;
 
         [Header("INPUT")] [Tooltip("Makes all Input snap to an integer. Prevents gamepads from walking slowly. Recommended value is true to ensure gamepad/keybaord parity.")]
-        public bool SnapInput = true;
+        public bool SnapInput = false;
 
         [Tooltip("Minimum input required before you mount a ladder or climb a ledge. Avoids unwanted climbing using controllers"), Range(0.01f, 0.99f)]
-        public float VerticalDeadZoneThreshold = 0.3f;
+        public float VerticalDeadZoneThreshold = 0.4f;
 
         [Tooltip("Minimum input required before a left or right is recognized. Avoids drifting with sticky controllers"), Range(0.01f, 0.99f)]
-        public float HorizontalDeadZoneThreshold = 0.1f;
+        public float HorizontalDeadZoneThreshold = 0.4f;
 
         [Header("MOVEMENT")] [Tooltip("The top horizontal movement speed")]
-        public float MaxSpeed = 14;
+        public float MaxSpeed = 25;
 
         [Tooltip("The player's capacity to gain horizontal speed")]
-        public float Acceleration = 120;
+        public float Acceleration = 200;
 
         [Tooltip("The pace at which the player comes to a stop")]
-        public float GroundDeceleration = 60;
+        public float GroundDeceleration = 500;
 
         [Tooltip("Deceleration in air only after stopping input mid-air")]
-        public float AirDeceleration = 30;
+        public float AirDeceleration = 50;
 
         [Tooltip("A constant downward force applied while grounded. Helps on slopes"), Range(0f, -10f)]
-        public float GroundingForce = -1.5f;
+        public float GroundingForce = -5.2f;
 
         [Tooltip("The detection distance for grounding and roof detection"), Range(0f, 0.5f)]
-        public float GrounderDistance = 0.05f;
+        public float GrounderDistance = 0.055f;
 
         [Header("JUMP")] [Tooltip("The immediate velocity applied when jumping")]
-        public float JumpPower = 36;
+        public float JumpPower = 50;
 
         [Tooltip("The maximum vertical movement speed")]
         public float MaxFallSpeed = 120;
@@ -55,6 +55,8 @@ namespace TController
         public float JumpBuffer = .2f;
 
         #endregion
+
+        [Header("ABILITIES")]
 
         #region Flame
 
